eec1ba8cfb6cc81664c6558bf25af6a63c29af50
Abdelrhman03 <109297166+Abdelrhman03@users.noreply.github.com>
2022-09-29 21:56:24 +0200

    Add files via upload

---
+++ app/Controllers/index.php
@@ -0,0 +1,30 @@
+<?php 
+    
+
+    // PDO function to connect the php with data base 
+    // we use try and catch to be sure that the php and 
+    // mysql can connected togther
+   
+
+    // how call data from data base to my page 
+    // QueryBuilder::insert("tasks", [
+    //     "task" => "I Want To Read Atomi Habit Book",
+    //     "description" => "I want to finish Atomic Habbit book duirng this week",
+    //     "completed" => 0,
+    // ]); 
+    // how to insert element using querry selector
+    
+
+    // QueryBuilder::delete("tasks", [
+    //     "task" => "I Want To Read Atomi Habit Book"
+    // ]); how to delete Data 
+    
+    // QueryBuilder::update("tasks" , [
+    //     "description" => "I want to finish ElDaa Wal Dawaa book duirng this week",
+    //     "completed" => 1,
+    // ], 4);  how to update data into the table
+
+    // QueryBuilder::delete("tasks", 1); 
+    // show to delete item  delete method take 4 parameter two is optional , (name of table , id of row , optional column , operatror)
+
+    // echo "<pre>"; // this html element will make object look good when insert it 
